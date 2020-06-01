27cd6551e81739ecb59a3b7a931fcf85bdc16199
ButchQiuQiu <133465202@qq.com>
2020-06-01 16:36:06 +0800

    SpringBus

--- spring_cloud_demo/spring_cloud_stream_kafka_p1_7888/src/main/java/com/bucth/spring_cloud_stream_kafka_p1_7888/service/MySource.java
+++ spring_cloud_demo/spring_cloud_stream_kafka_p1_7888/src/main/java/com/bucth/spring_cloud_stream_kafka_p1_7888/service/MySource.java
@@ -1,9 +1,7 @@
 package com.bucth.spring_cloud_stream_kafka_p1_7888.service;
 
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 
 /**
  * 自定义管道类·································
